00a24c4ce0f48fb6b97bd07e46efd1e4475e86f6
CarloQuinteros <carloq24@gmail.com>
2025-05-13 13:06:12 -0400

    Modification: new routes

--- miniMessageBoard/routes/index.js
+++ miniMessageBoard/routes/index.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
-const messageController=require('../controllers/messageController');
+//const messageController=require('../controllers/messageController');
+const messageController=require('../controllers/messageControllerv2');
 
-router.get('/',messageController.messageIndex);
+router.get('/',messageController.getMessages);
 
 
 module.exports=router;
--- miniMessageBoard/routes/newMessage.js
+++ miniMessageBoard/routes/newMessage.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
-const newDataController=require('../controllers/newDataController');
+//const newDataController=require('../controllers/newDataController');
+const newDataController=require('../controllers/messageControllerv2');
 
-router.get('/',newDataController.newData);
-router.post('/',newDataController.create_message);
+router.get('/',newDataController.newMessage);
+router.post('/',newDataController.createMessagePost);
 
 
 module.exports=router;
